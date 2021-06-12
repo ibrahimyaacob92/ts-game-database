a12ae9476ed759cf4a2304df811660581a6392a3
Ibrahim Yaacob <ibrahimyaacob92@yahoo.com>
2021-06-12 19:41:41 +0800

    create simple components

---
+++ src/components/App/index.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "components/Home";
+
+const App = (): ReactElement => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/">
+        hello
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
+
+export default App;
